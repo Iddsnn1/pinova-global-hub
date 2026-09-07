ba3a3c49f2cda7bca7a2f33315f6804e19d94592
Iddsnn1 <120267537+Iddsnn1@users.noreply.github.com>
2026-09-07 04:54:41 +0100

    Use environment variable for server port

--- server.ts
+++ server.ts
@@ -38,7 +38,7 @@ import { EducationRepository } from './src/server/db/repositories/EducationRepos
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
