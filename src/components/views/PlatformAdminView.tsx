a001599ecef02d68213fb862f87d7c1b6e70fd7d
Iddsnn1 <120267537+Iddsnn1@users.noreply.github.com>
2026-09-24 13:58:33 +0100

    fix(pi): remove invalid formatter imports

--- src/components/views/PlatformAdminView.tsx
+++ src/components/views/PlatformAdminView.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useMemo } from 'react';
 import { formatPiAmount } from '../../utils/formatters';
-import { formatPiAmount } from './utils/formatters';
 import { 
   ShieldCheck, 
   Activity, 
