eb5473c363124753e7401c246dc13bcb5647c387
Iddsnn1 <120267537+Iddsnn1@users.noreply.github.com>
2026-09-24 13:57:03 +0100

    fix(pi): add precise amount formatter imports

--- src/components/views/PlatformAdminView.tsx
+++ src/components/views/PlatformAdminView.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useMemo } from 'react';
+import { formatPiAmount } from '../../utils/formatters';
 import { formatPiAmount } from './utils/formatters';
 import { 
   ShieldCheck, 
